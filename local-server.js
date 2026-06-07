4a7e56ac229d852e29d82d1fe545b31f0e7f5f8e
{Meher} <{meherpra5@gmail.com}>
2026-06-08 00:39:07 +0530

    Allow connections from local network in server

--- local-server.js
+++ local-server.js
@@ -43,6 +43,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(port, "127.0.0.1", () => {
-  console.log(`CSE357 app running at http://127.0.0.1:${port}/`);
+server.listen(port, "0.0.0.0", () => {
+  console.log(`CSE357 app running at http://0.0.0.0:${port}/`);
 });
